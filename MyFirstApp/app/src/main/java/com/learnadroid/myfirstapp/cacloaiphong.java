8060a42ed4eaea0c1f0745bc37cc4453fd177a97
Trần Xuân Trường <55401276+truong09@users.noreply.github.com>
2020-05-29 00:40:06 +0700

    commit

---
+++ MyFirstApp/app/src/main/java/com/learnadroid/myfirstapp/cacloaiphong.java
@@ -0,0 +1,14 @@
+package com.learnadroid.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cacloaiphong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cacloaiphong);
+    }
+}
